6e6b1f0d01d3881cae070ec1e02bc64c986e17da
jude-LafiteIII <judewrs@gmail.com>
2018-10-12 23:45:03 +0100

    looping and changes

--- src/main.rs
+++ src/main.rs
@@ -8,43 +8,61 @@ use chrono_tz::Tz;
 use chrono::prelude::*;
 use std::collections::HashMap;
 use std::env;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     dotenv::dotenv().ok();
 
-    let TOKEN = env::var("TOKEN").unwrap();
+    let token = env::var("TOKEN").unwrap();
+    let sql_url = env::var("SQL").unwrap();
+
     const URL: &str = "https://discordapp.com/api/v6";
 
-    let mut c = mysql::Conn::new("mysql://root:testpassword@localhost/timezone").unwrap();
+    let mut c = mysql::Conn::new(sql_url).unwrap();
     let client = reqwest::Client::new();
 
-    let q = c.query("SELECT * FROM clocks").unwrap();
+    loop {
+        let q = c.query("SELECT * FROM clocks").unwrap();
 
-    for res in q {
-        let (_id, channel_id, timezone, channel_name, _guild_id, message_id) = mysql::from_row::<(u32, u64, String, String, u64, Option<u64>)>(res.unwrap());
+        for res in q {
+            let (_id, channel_id, timezone, channel_name, _guild_id, message_id) = mysql::from_row::<(u32, u64, String, String, u64, Option<u64>)>(res.unwrap());
 
-        let t: Tz = timezone.parse().unwrap();
-        let dt = Utc::now().with_timezone(&t);
+            let t: Tz = timezone.parse().unwrap();
+            let dt = Utc::now().with_timezone(&t);
 
-        if let Some(m_id) = message_id {
-            let mut m = HashMap::new();
-            m.insert("content", dt.format(&channel_name).to_string());
+            if let Some(m_id) = message_id {
+                let mut m = HashMap::new();
+                m.insert("content", dt.format(&channel_name).to_string());
 
-            client.patch(&format!("{}/channels/{}/messages/{}", URL, channel_id, m_id))
-                .json(&m)
-                .header("Content-Type", "application/json")
-                .header("Authorization", format!("Bot {}", TOKEN))
-                .send().unwrap();
+                match client.patch(&format!("{}/channels/{}/messages/{}", URL, channel_id, m_id))
+                    .json(&m)
+                    .header("Content-Type", "application/json")
+                    .header("Authorization", format!("Bot {}", token))
+                    .send() {
+
+                    Err(_) => println!("Error value occured"),
+
+                    _ => {}
+                }
+            }
+            else {
+                let mut m = HashMap::new();
+                m.insert("name", dt.format(&channel_name).to_string());
+
+                match client.patch(&format!("{}/channels/{}", URL, channel_id))
+                    .json(&m)
+                    .header("Content-Type", "application/json")
+                    .header("Authorization", format!("Bot {}", token))
+                    .send() {
+
+                    Err(_) => println!("Error value occured"),
+
+                    _ => {}
+                }
+            }
         }
-        else {
-            let mut m = HashMap::new();
-            m.insert("name", dt.format(&channel_name).to_string());
 
-            client.patch(&format!("{}/channels/{}", URL, channel_id))
-                .json(&m)
-                .header("Content-Type", "application/json")
-                .header("Authorization", format!("Bot {}", TOKEN))
-                .send().unwrap();
-        }
+        thread::sleep(Duration::from_secs(1));
     }
 }
