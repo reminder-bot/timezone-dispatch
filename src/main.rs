c8ea5b57f9ade3cd74c9b954f119c653610ac236
jude-LafiteIII <judewrs@gmail.com>
2018-10-16 21:28:54 +0100

    removing 404s

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-extern crate mysql;
+#[macro_use] extern crate mysql;
+
 extern crate dotenv;
 extern crate chrono;
 extern crate chrono_tz;
@@ -12,6 +13,7 @@ use std::env;
 use std::thread;
 use std::time::Duration;
 
+
 fn main() {
     dotenv::dotenv().ok();
 
@@ -20,40 +22,46 @@ fn main() {
 
     const URL: &str = "https://discordapp.com/api/v6";
 
-    let mut connection = mysql::Conn::new(sql_url).unwrap();
+    let mysql_conn = mysql::Pool::new(sql_url).unwrap();
     let req_client = reqwest::Client::new();
     let pool = threadpool::ThreadPool::new(8);
 
     loop {
-        let q = connection.query("SELECT * FROM clocks ORDER BY RAND()").unwrap();
-        let mut requests: Vec<reqwest::RequestBuilder> = vec![];
+        let q = mysql_conn.prep_exec("SELECT * FROM clocks ORDER BY RAND()", ()).unwrap();
 
         for res in q {
-            let (_id, channel_id, timezone, channel_name, _guild_id, message_id) = mysql::from_row::<(u32, u64, String, String, u64, Option<u64>)>(res.unwrap());
+            let (id, channel_id, timezone, channel_name, _guild_id, message_id) = mysql::from_row::<(u32, u64, String, String, u64, Option<u64>)>(res.unwrap());
 
             let t: Tz = timezone.parse().unwrap();
             let dt = Utc::now().with_timezone(&t);
+
+            let mut req;
 
             if let Some(m_id) = message_id {
                 let mut m = HashMap::new();
                 m.insert("content", dt.format(&channel_name).to_string());
 
-                requests.push(send(format!("{}/channels/{}/messages/{}", URL, channel_id, m_id), &m, &token, &req_client));
+                req = send(format!("{}/channels/{}/messages/{}", URL, channel_id, m_id), &m, &token, &req_client);
             }
             else {
                 let mut m = HashMap::new();
                 m.insert("name", dt.format(&channel_name).to_string());
 
-                requests.push(send(format!("{}/channels/{}", URL, channel_id), &m, &token, &req_client));
+                req = send(format!("{}/channels/{}", URL, channel_id), &m, &token, &req_client);
             }
-        }
 
-        for req in requests {
+            let conn = mysql_conn.clone();
             pool.execute(move || {
                 match req.send() {
-                    Err(r) => println!("{:?}", r.status()),
+                    Err(_) => {},
 
-                    Ok(r) => println!("{:?}", r.status())
+                    Ok(r) => {
+                        if r.status() == 404 {
+                            println!("Delete required");
+                            println!("ID: {}", id);
+                            conn.prep_exec("DELETE FROM clocks WHERE id = :id", params!{"id" => id});
+                        }
+                    }
                 }
             });
         }
